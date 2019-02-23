1c6f316792d7426f49e861be362daaee72cbf0f6
Fernando Tavares <47903622+fenoliv@users.noreply.github.com>
2019-02-22 23:09:53 -0300

    resolvido

--- Index.php
+++ Index.php
@@ -1,11 +1,8 @@
 <?php
 
-<<<<<<< HEAD
    echo"É muito Dificil 456";
-=======
-   echo"É muito Dificil 1020";
 
    echo"testando o GIT";
->>>>>>> 715ac0abe1b1727f8762eaa9843d9f8f414829c9
+
 
    ?>
